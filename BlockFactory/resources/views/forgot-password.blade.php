185872a5520a2abd71f8bbefe0fe5c3f85e14734
AruberutoEXE <albertoblsr@gmail.com>
2023-03-30 16:54:30 +0200

    vistas agregadas
    vistas de ejemplo añadidas

---
+++ BlockFactory/resources/views/forgot-password.blade.php
@@ -0,0 +1,46 @@
+@extends('auth.template')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        @if(session('status'))
+        <div class="alert alert-success" role="alert">
+            {{session('status')}}
+        </div>
+        @endif
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Reset password') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.request') }}">
+                        @csrf
+
+                        <div class="form-group row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+            
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Send Email') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
