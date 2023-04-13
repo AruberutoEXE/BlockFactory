d71c518d30d58dda3484fe07b22a799fb4094c49
st99sp <spmsevilla@gmail.com>
2023-04-14 01:01:01 +0200

    Mejora de la vista

--- BlockFactory/resources/views/producto/indexUser.blade.php
+++ BlockFactory/resources/views/producto/indexUser.blade.php
@@ -5,51 +5,52 @@ producto
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-header">
+          <div style="display: flex; justify-content: space-between; align-items: center;">
 
-                            <span id="card_title">
-                                {{ __('LISTADO DE PRODUCTOS EN NUESTRA TIENDA ') }}
-                            </span>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-                    <div class="container">
- @foreach ($productos as $producto)
-<div class="row">
-  <div class="col-md-4">
-    <div class="card mb-4 box-shadow">
-    {{ $foto=$producto->id}}
-      <img class="card-img-top" src="2.png" alt="Card image cap">
-      <div class="card-body">
-        <p class="card-text">{{ $producto->descripcion }}</p>
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-          <a class="btn btn-sm btn-primary " href="{{ route('productos.show',$producto->id) }}"><i class="fa fa-fw fa-eye"></i> Ver más</a>
+            <span id="card_title">
+              {{ __('LISTADO DE PRODUCTOS EN NUESTRA TIENDA ') }}
+            </span>
           </div>
-           
-          <span class="text-muted">{{ $producto->nombre }}</span>
-          <span class="text-muted">{{ $producto->precio }} €</span>
+        </div>
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+          <p>{{ $message }}</p>
+        </div>
+        @endif
 
+        <div class="album py-5 bg-light">
+
+          <div class="container">
+            <div class="row">
+              @foreach ($productos as $producto)
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="img/{{$producto->id}}.png" alt="Card image cap">
+                  <div class="card-body">
+                    <p class="card-text">{{ $producto->descripcion }}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a class="btn btn-sm btn-primary " href="{{ route('productos.show',$producto->id) }}"><i class="fa fa-fw fa-eye"></i> Ver más</a>
+                      </div>
+
+                      <span class="">{{ $producto->nombre }}</span>
+                      <span class="text-muted">{{ $producto->precio }} €</span>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              @endforeach
+
+            </div>
+          </div>
+          {!! $productos->links() !!}
         </div>
       </div>
     </div>
-  </div>
-</div>
-@endforeach
- 
-                </div>
-                {!! $productos->links() !!}
-            </div>
-        </div>
-    </div>
-@endsection
-
+    @endsection
