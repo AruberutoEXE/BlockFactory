64b72d7737b12ae94cd096c1414fc40176164683
st99sp <spmsevilla@gmail.com>
2023-05-02 21:44:05 +0200

    commit2

---
+++ BlockFactory/app/Http/Controllers/FavoritosController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\favorito;
+use App\Models\Producto;
+
+use Illuminate\Support\Facades\Auth;
+class FavoritosController extends Controller
+{
+    
+    public function add(Request $request){
+
+        if(!empty(Auth::id())){
+            favorito::create(['producto_id'=>$request->id,'user_id'=>Auth::id()]);
+            $producto = Producto::find($request->id);
+            $fav=false;
+            if(!empty(Auth::id())){
+                $favorito=favorito::where('producto_id',$producto->id);
+                if(!empty($favorito)){
+                    $favorito=$favorito->where('user_id', Auth::id())->first();
+                    if(!empty($favorito)){
+                        $fav=true;
+                    }
+                }
+            }
+            return view('producto.show', compact(['producto','fav']));
+        }else{
+            return view('Auth.login');
+        }
+    }
+    public function remove(Request $request)
+    {
+        
+        if(!empty(Auth::id())){
+            $linea=favorito::where(['producto_id'=>$request->id,'user_id'=>Auth::id()])->first();
+            $linea->delete();
+            $producto = Producto::find($request->id);
+            $fav=false;
+            if(!empty(Auth::id())){
+                $favorito=favorito::where('producto_id',$producto->id);
+                if(!empty($favorito)){
+                    $favorito=$favorito->where('user_id', Auth::id())->first();
+                    if(!empty($favorito)){
+                        $fav=true;
+                    }
+                }
+            }
+            return view('producto.show', compact(['producto','fav']));
+        }else{
+            return view('Auth.login');
+        }
+    }
+}
